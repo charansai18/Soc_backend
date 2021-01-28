434ae44a3d2b94d2968987ff9ba8e38bf435619d
Charan Sai <acs13@iitbbs.ac.in>
2021-01-28 18:50:53 +0530

    ok

--- index.js
+++ index.js
@@ -118,6 +118,7 @@ MongoClient.connect('mongodb+srv://Registration:charangoc30@cluster0.fga0d.mongo
         }) ,
           (req, res) => {
               console.log("login done");
+              res.setHeader('Content-Type', 'application/json');
               res.redirect('http://localhost:3000/admin/dashboard', 200,  {userData:user});
              
           }
